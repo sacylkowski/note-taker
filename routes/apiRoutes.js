aa9ac29cb63758ef3bc565e174846b01a93591a3
sacylkowski <sacylkowski@gmail.com>
2022-04-20 14:18:19 -0500

    Get and Post API routes are working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,13 @@
 const path = require("path");
 const fs = require("fs");
 const router = require("express").Router();
+const uuid = require("uuid");
 
-router.get("/api/notes", (req, res) => {
+// router.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../db/db.json"));
+// });
+
+router.get("/notes", (req, res) => {
     fs.readFile("db/db.json", (err, data) => {
         if (err) {
             res.send(404);
@@ -13,24 +18,19 @@ router.get("/api/notes", (req, res) => {
     })
 });
 
-// router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    notes.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(notes);
+});
 
-//     if 
-// });
 
 // router.delete("/api/notes/:id", (req, res) => {
 
 // });
-
-
-// const saveNote = (note) =>
-//   fetch('/api/notes', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(note),
-//   });
 
 // const deleteNote = (id) =>
 //   fetch(`/api/notes/${id}`, {
